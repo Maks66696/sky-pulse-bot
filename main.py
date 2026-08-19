1de0ffaaabe520c656c56b9d8d58222c515cf37e
Maks66696 <koryakin_10@internet.ru>
2026-08-19 12:00:26 +0500

    fix(deploy): добавлен aiohttp health-check сервер для порта Render

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from bot.handlers import start, weather, location
 import asyncio
 import logging
-from config import config
+import os
+from aiohttp import web
 from aiogram import Bot, Dispatcher
+from config import config
 from bot.database.db import init_db
-
+from bot.handlers import start, weather, location
 
 
 logging.basicConfig(level=logging.INFO)
 
+async def handle_ping(request):
+    return web.Response(text="🌤️ SkyPulse Bot is alive and running 24/7!")
 
 async def main():
+    
+    app = web.Application()
+    app.router.add_get("/", handle_ping)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.getenv("PORT", 8080))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+
     bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(start.router)
     dp.include_router(location.router)
     dp.include_router(weather.router)
+
     await init_db()
     await dp.start_polling(bot)
 
