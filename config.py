538f8a0013cfc572ca8c337e9cbe35f6ba0bcd7b
Maks66696 <koryakin_10@internet.ru>
2026-08-20 18:57:03 +0500

    fix: config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ load_dotenv()
 class Config:
     BOT_TOKEN: str = os.getenv("BOT_TOKEN")
     WEATHER_API_KEY: str = os.getenv("WEATHER_API_KEY")
-    ADMIN_ID: int = int(os.getenv("ADMIN_ID"), 0)
+    ADMIN_ID: int = int(os.getenv("ADMIN_ID", 0))
 
 config = Config()
